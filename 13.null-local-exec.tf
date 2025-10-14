4e54e38955c1da3d716d931a1e76a68f7c5e83f2
VjD21 <vijaydurai379@gmail.com>
2025-10-14 14:01:01 +0530

    fix: corrected null_resource local-exec with loop

--- 13.null-local-exec.tf
+++ 13.null-local-exec.tf
@@ -1,12 +1,20 @@
 resource "null_resource" "webservers" {
   provisioner "local-exec" {
     command = <<EOH
-      sleep 60
-      ansible -i invfile pvt -m ping
+      for i in {1..12}; do
+        echo "Checking SSH connectivity..."
+        ansible -i invfile pvt -m ping && break
+        echo "Retrying in 10s..."
+        sleep 10
+      done
     EOH
   }
-  depends_on = [local_file.ansible-inventory-file]
+
+  depends_on = [
+    local_file.ansible-inventory-file
+  ]
 }
+
 
 
 
